49c40543dd4a86c31bfadcad43b61f9121120ea1
Dmucci <dmucci93@gmail.com>
2023-10-21 18:56:18 +0200

    finished

--- js/app.js
+++ js/app.js
@@ -1,1 +1,30 @@
 // Tu código aquí.
+let count = 0
+
+btnAdd.addEventListener('click', function () {
+    count++
+    document.querySelector("#counter").textContent = count
+    if (count>0) {
+        document.querySelector("#counter").style.color = "green";
+    } else if (count<0) {
+        document.querySelector("#counter").style.color = "red";
+    } else {
+        document.querySelector("#counter").style.color = "black";
+    }
+})
+
+btnLower.addEventListener('click', function(){
+    count--
+    document.querySelector('#counter').textContent = count
+    if (count>0) {
+        document.querySelector("#counter").style.color = "green";
+    } else if (count<0) {
+        document.querySelector("#counter").style.color = "red";
+    } else {
+        document.querySelector("#counter").style.color = "black";
+    }
+})
+
+  
+
+
